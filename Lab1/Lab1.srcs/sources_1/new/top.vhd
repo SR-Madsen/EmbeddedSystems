03b88580364dabbfaa3e1ac6b19eaff2519cd17a
SR-Madsen <ligthflash@gmail.com>
2021-02-17 11:45:28 +0100

    Added progress for Lab 1 of Embedded Systems. Still needs row entity added, as well as simulations.

---
+++ Lab1/Lab1.srcs/sources_1/new/top.vhd
@@ -0,0 +1,67 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( clk_8ns : in STD_LOGIC;
+           led : out STD_LOGIC_VECTOR (3 downto 0));
+           --blue : out STD_LOGIC_VECTOR (7 downto 0);
+           --red : out STD_LOGIC_VECTOR (7 downto 0);
+           --green : out STD_LOGIC_VECTOR (7 downto 0);
+           --row : out STD_LOGIC_VECTOR (7 downto 0));
+end top;
+
+architecture Behavioral of top is
+
+component timer is
+    Port ( clk : in STD_LOGIC;
+           count : out STD_LOGIC_VECTOR (7 downto 0));
+end component;
+
+--component pwm_generator is
+--    Port ( counter : in STD_LOGIC_VECTOR (7 downto 0);
+--           ocr : in STD_LOGIC_VECTOR (7 downto 0);
+--           pwm : out STD_LOGIC);
+--end component;
+
+component color_channel is
+    Port ( counter : in STD_LOGIC_VECTOR (7 downto 0);
+           red : in STD_LOGIC_VECTOR (7 downto 0);
+           green : in STD_LOGIC_VECTOR (7 downto 0);
+           blue : in STD_LOGIC_VECTOR (7 downto 0);
+           red_pwm : out STD_LOGIC;
+           green_pwm : out STD_LOGIC;
+           blue_pwm : out STD_LOGIC);
+end component;
+
+signal count_out: STD_LOGIC_VECTOR(7 downto 0);
+signal ocr_red: STD_LOGIC_VECTOR(7 downto 0) := "11111111";
+signal ocr_green: STD_LOGIC_VECTOR(7 downto 0) := "00100000";
+signal ocr_blue: STD_LOGIC_VECTOR(7 downto 0) := "00000001";
+
+begin
+
+    timer1: timer port map (clk => clk_8ns,
+                            count => count_out);
+                            
+--    pwm_generator1: pwm_generator port map (counter => count_out,
+--                                            ocr => ocr_static,
+--                                            pwm => led(0));
+
+    color_channel1: color_channel port map (counter => count_out,
+                                            red => ocr_red,
+                                            green => ocr_green,
+                                            blue => ocr_blue,
+                                            red_pwm => led(0),
+                                            green_pwm => led(1),
+                                            blue_pwm => led(2));
+
+end Behavioral;
